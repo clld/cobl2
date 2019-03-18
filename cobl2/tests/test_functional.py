9dbf786972fca186dc60202b068c51b4e5dff0a3
Hans-Jörg Bibiko <bibiko@shh.mpg.de>
2019-03-18 11:15:16 +0100

    added tests for customized meaning column in cognatesets #2

--- cobl2/tests/test_functional.py
+++ cobl2/tests/test_functional.py
@@ -7,6 +7,8 @@ pytest_plugins = ['clld']
     "method,path",
     [
         ('get_html', '/'),
+        ('get_dt', '/cognatesets?iSortingCols=1&iSortCol_0=1'),
+        ('get_dt', '/cognatesets?sSearch_1=right'),
     ])
 def test_pages(app, method, path):
     getattr(app, method)(path)
